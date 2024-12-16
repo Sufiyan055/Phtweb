b44a706a32c6941e76849e83322f9d1c192f7852
Sufiyan055 <sufiyan007882@gmail.com>
2024-12-16 22:02:09 +0530

    phtweb

--- js/testinomial.js
+++ js/testinomial.js
@@ -1,22 +1,22 @@
 const testinomials = [
   {
     name: "Jessica R.",
-    image: "../assets/StImg1.webp",
+    image: "assets/StImg1.webp",
     testinomial: `"I cannot express how happy we are with the photos from our wedding day! Every moment was captured beautifully, and the attention to detail was incredible. Looking through the pictures feels like reliving the day all over again. Highly recommend!"`,
   },
   {
     name: "Daniel T.",
-    image: "../assets/danial.jpg",
+    image: "assets/danial.jpg",
     testinomial: `"I hired them to cover my 30th birthday party, and the results were phenomenal. From candid shots of my friends laughing to posed group photos, every picture told a story. These are memories I will cherish forever!"`,
   },
   {
     name: "Emily and Mark L.",
-    image: "../assets/StImg4.webp",
+    image: "assets/StImg4.webp",
     testinomial: `"We wanted a unique holiday card this year, and the photographer exceeded our expectations. The lighting, the background, and the overall vibe were just perfect. We received so many compliments on the photos. Thank you for your creativity!"`,
   },
   {
     name: "Michael P., Event Coordinator",
-    image: "../assets/StImg2.webp",
+    image: "assets/StImg2.webp",
     testinomial: `"Our company’s annual gala was captured flawlessly. From the vibrant décor to the happy faces of our guests, every photo turned out amazing. It’s not easy to make a corporate event look so magical, but you nailed it!"`,
   },
 ];
